f8eab2b29377c56fb69b1ebb528c1f154f285ffd
Sukolen Vadym <sukolenvo@gmail.com>
2019-08-10 22:30:49 +0800

    mouse rotation

--- src/main/java/com/dakare/rubik/MainController.java
+++ src/main/java/com/dakare/rubik/MainController.java
@@ -3,12 +3,12 @@ package com.dakare.rubik;
 import com.dakare.rubik.model.ColorsHelper;
 import com.dakare.rubik.view.CubeView;
 import javafx.fxml.FXML;
-import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.SubScene;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,6 +16,10 @@ public class MainController {
 
     @FXML
     private Group root;
+    private Rotate rotateVertical;
+    private Rotate rotateHorizontal;
+    private double sceneX;
+    private double sceneY;
 
     @FXML
     private void initialize() {
@@ -23,19 +27,40 @@ public class MainController {
         drawLayer(root, -1);
         drawLayer(root, 0);
         drawLayer(root, 1);
+        rotateVertical = new Rotate(45, Rotate.X_AXIS);
+        root.getTransforms().add(rotateVertical);
+        rotateHorizontal = new Rotate(45, Rotate.Y_AXIS);
+        root.getTransforms().add(rotateHorizontal);
 
         PerspectiveCamera camera = new PerspectiveCamera(true);
         camera.setTranslateZ(-10 * CubeView.SIZE);
-        camera.setTranslateY(-5 * CubeView.SIZE);
-        camera.setTranslateX(5 * CubeView.SIZE);
-        camera.setRotate(30);
-        camera.setRotationAxis(new Point3D(-1, -1, 0));
         camera.setFarClip(300);
-        root.getChildren().add(camera);
+        this.root.getChildren().add(camera);
         SubScene subScene = new SubScene(root, 800, 600, true, SceneAntialiasing.DISABLED);
         subScene.setCamera(camera);
         subScene.setFill(Color.grayRgb(150));
+        handleMouse(subScene);
         this.root.getChildren().add(subScene);
+    }
+
+    private void handleMouse(SubScene subScene) {
+        subScene.setOnMousePressed(event -> {
+            sceneX = event.getSceneX();
+            sceneY = event.getSceneY();
+        });
+
+        subScene.setOnMouseDragged(event -> {
+            double diffX = event.getSceneX() - sceneX;
+            double diffY = event.getSceneY() - sceneY;
+            if (Math.abs(diffX) > 1) {
+                rotateHorizontal.setAngle((rotateHorizontal.getAngle() - diffX) * 1);
+                sceneX = event.getSceneX();
+            }
+            if (Math.abs(diffY) > 1) {
+                rotateVertical.setAngle((rotateVertical.getAngle() + diffY) * 1);
+                sceneY = event.getSceneY();
+            }
+        });
     }
 
     private void drawLayer(Group root, int depth) {
